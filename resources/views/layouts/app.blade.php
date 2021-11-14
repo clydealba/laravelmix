444188b040b73044fc86fe8f861661186418c114
clydealba <clyde@albacars.ae>
2021-11-14 15:09:27 +0400

    styled the initial layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,35 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex itens-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+
+        <ul class="flex itens-center">
+            <li>
+                <a href="" class="p-3">Clyde Manlosa Ibalez</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
